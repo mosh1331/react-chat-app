fa2a4effbc2b3d73f92fae1eb6d1fe7a61ad25de
m.shamil <m.shamil@umenit.com>
2022-10-01 14:30:04 +0530

    responsive

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -4,9 +4,9 @@ import { logout } from '../../services/firebase'
 const Header = ({user}) => {
     return (
         <div className="w-full h-[10vh] bg-indigo-800 flex justify-center items-center ">
-            <h2 className="text-white font-bold text-xl">Chat Room - React</h2>
+            <h2 className="text-white font-bold text-md md:text-xl">Chat Room - React</h2>
             {user ?
-         <button onClick={logout} className="bg-rose-600 rounded p-2 text-white font-bold absolute top-6 right-12 ">Signout</button>
+         <button onClick={logout} className="bg-rose-600 rounded p-2 text-sm md:text-l text-white font-bold absolute top-4 right-2 md:top-6 md:right-12 ">Signout</button>
         :null    
         }
         </div>
